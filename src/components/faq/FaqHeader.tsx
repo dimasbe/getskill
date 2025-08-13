af1ae4d670eed527c253d94c3f430ffee7a47be3
Mohkosim <mohkosim8014@gmail.com>
2025-08-13 09:12:25 +0700

    update commit 13.8.25

--- src/components/faq/FaqHeader.tsx
+++ src/components/faq/FaqHeader.tsx
@@ -2,23 +2,18 @@ import BackgroundShapes from "../public/BackgroundShapes";
 
 export default function FaqHeader() {
   return (
-    <div className="relative px-6 py-14 bg-gradient-to-r from-indigo-100 via-stone-100 to-fuchsia-100 overflow-hidden shadow-sm">
-      <BackgroundShapes />
-      <div className="max-w-6xl mx-auto px-4 sm:px-6 text-center sm:text-left relative z-10 animate-fadeIn">
-        <h1 className="text-3xl sm:text-4xl font-extrabold text-gray-800 tracking-tight">
-          FAQ
-        </h1>
-        <p className="mt-3 text-sm sm:text-base text-gray-700">
-          <a
-            href="/"
-            className="hover:underline hover:text-indigo-600 transition-colors duration-200"
-          >
-            Beranda
-          </a>
-          <span className="mx-1">&gt;</span>
-          <span className="text-purple-600 font-medium">FAQ</span>
-        </p>
+    <div className="relative px-6 py-11 bg-gradient-to-r from-indigo-100 via-stone-100 to-fuchsia-100 overflow-hidden">
+        <BackgroundShapes />
+        <div className="max-w-6xl mx-auto px-4 sm:px-18 text-center sm:text-left relative z-10">
+          <h1 className="text-2xl sm:text-3xl font-extrabold text-gray-800">FAQ</h1>
+          <p className="mt-2 text-sm sm:text-base text-gray-800">
+            <a href="/" className="hover:underline">
+              Beranda
+            </a>
+            <span className="mx-1">&gt;</span>
+            <span className="text-purple-600">FAQ</span>
+          </p>
+        </div>
       </div>
-    </div>
   );
 }
--- src/pages/guest/berita/Berita.tsx
+++ src/pages/guest/berita/Berita.tsx
@@ -51,7 +51,7 @@ const Berita: React.FC = () => {
       {/* Header */}
       <div className="relative px-6 py-11 bg-gradient-to-r from-indigo-100 via-stone-100 to-fuchsia-100 overflow-hidden">
         <BackgroundShapes />
-        <div className="max-w-6xl mx-auto px-4 sm:px-6 text-center sm:text-left relative z-10">
+        <div className="max-w-6xl mx-auto px-4 sm:px-18 text-center sm:text-left relative z-10">
           <h1 className="text-2xl sm:text-3xl font-extrabold text-gray-800">Berita</h1>
           <p className="mt-2 text-sm sm:text-base text-gray-800">
             <a href="/" className="hover:underline">
