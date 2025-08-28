f5a63b6fa4742a2a9a5547b6f826a902e406bb44
SandiAri <sndiari12@gmail.com>
2025-08-28 10:03:54 +0700

    kelas industri

--- src/components/kelasindustri/HeroSection.tsx
+++ src/components/kelasindustri/HeroSection.tsx
@@ -12,11 +12,11 @@ const HeroSection: React.FC = () => {
   return (
     <section
       className="relative overflow-hidden bg-cover bg-center 
-                py-2 md:py-12 lg:py-31"
+                py-2 md:py-12 lg:py-31 xl:py-19"
       style={{ backgroundImage: `url(${backgroundClass})` }}
     >
       <div
-        className="container mx-auto px-4 sm:px-6 md:px-12 lg:px-16
+        className="container mx-auto px-4 sm:px-6 md:px-12 lg:px-16 xl:px-20
              flex flex-col items-center justify-between 
              gap-8 md:gap-12 lg:flex-row"
       >
@@ -26,18 +26,19 @@ const HeroSection: React.FC = () => {
           transition={{ duration: 1, ease: "easeOut" }}
           className="
               w-full text-center space-y-1 relative z-20           
-              md:space-y-4 md:pl-8 md:text-left   /* 🔹 Tablet */
-              lg:w-7/12 lg:text-left lg:absolute lg:top-1/3 lg:left-12 /* 🔹 Desktop */
+              md:space-y-4 md:pl-8 md:text-left   
+              lg:w-7/12 lg:text-left lg:absolute lg:top-1/3 lg:left-12 
+              xl:top-1/4 xl:left-20 xl:w-6/12
   "
         >
           {/* Heading */}
           <h3
             className="
                 text-2xl font-semibold text-gray-800 leading-snug tracking-tight
-                sm:text-3xl  /* Tablet kecil */
-                md:text-4xl md:leading-snug  /* Tablet sedang */
-                lg:text-[47px] lg:leading-[1.2]   /* Desktop */
-                xl:text-[50px] xl:leading-[1.2]  /* Desktop besar */
+                sm:text-3xl 
+                md:text-4xl md:leading-snug  
+                lg:text-[47px] lg:leading-[1.2]   
+                xl:text-[50px] xl:leading-[1.2]
               "
           >
             Selamat Datang Di Kelas{" "}
@@ -51,9 +52,10 @@ const HeroSection: React.FC = () => {
             id="header-description"
             className="
                 mx-auto max-w-md text-sm text-gray-600          
-                sm:text-base /* Tablet kecil */    
-                md:max-w-xl md:text-sm  /* Tablet sedang */            
-                lg:mx-0 lg:-mt-5 /* Desktop */
+                sm:text-base 
+                md:max-w-2xl md:text-sm md:-mt-5
+                lg:mx-0 lg:-mt-5 
+                xl:text-sm xl:max-w-2xl xl:-mt-5
               "
           >
             Belajar seru bersama GetSkill
@@ -62,8 +64,7 @@ const HeroSection: React.FC = () => {
 
         <div
           className="relative flex w-full items-center justify-center overflow-visible
-                     /* Desktop */
-                     lg:w-6/12"
+                     lg:w-6/12 xl:w-7/12"
         >
           <motion.img
             src={peopleBanner}
@@ -73,9 +74,10 @@ const HeroSection: React.FC = () => {
             transition={{ duration: 1.2, ease: "easeOut" }}
             className="
               relative z-10 drop-shadow-xl scale-95 top-4              
-              sm:top-6 sm:scale-100  /* Tablet kecil */             
-              md:top-8  /* Tablet sedang */              
-              lg:top-10 lg:translate-x-130 lg:scale-155  /* Desktop */
+              sm:top-6 sm:scale-100               
+              md:top-8              
+              lg:top-10 lg:translate-x-130 lg:scale-155  
+              xl:top-15 xl:translate-x-130 xl:scale-110
             "
             style={{ width: "80%", maxWidth: "500px" }}
           />
@@ -90,9 +92,10 @@ const HeroSection: React.FC = () => {
             className="
               absolute -z-0 top-32
               w-[85%] max-w-[360px]                
-              sm:top-40 sm:scale-159   /* Tablet kecil */
-              md:top-75 /* Tablet sedang */
-              lg:top-51 lg:scale-159 lg:translate-x-120 /* Desktop - lebih besar */
+              sm:top-40 sm:scale-159   
+              md:top-75
+              lg:top-51 lg:scale-159 lg:translate-x-120 
+              xl:top-76 xl:scale-159 xl:translate-x-120
             "
 
           />
@@ -107,9 +110,10 @@ const HeroSection: React.FC = () => {
         transition={{ duration: 1.3, ease: "easeOut", delay: 1 }}
         className="
           absolute left-0 top-2 z-30 opacity-80 w-[100px]          
-          sm:top-3 sm:w-[130px]  /* Tablet kecil */          
-          md:w-[160px]  /* Tablet sedang */          
-          lg:w-[210px]  /* Desktop */
+          sm:top-3 sm:w-[130px]  
+          md:w-[160px]  
+          lg:w-[210px]  
+          xl:w-[210px] xl:top-6
         "
       />
     </section>
