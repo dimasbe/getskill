6eb630c6a98cd05743aca9963bed5928440fa3ea
dimasbe <bedimas55@gmail.com>
2025-08-29 14:50:46 +0700

    fix berita

--- src/components/public/CardNews/RelatedNews.tsx
+++ src/components/public/CardNews/RelatedNews.tsx
@@ -13,12 +13,12 @@ interface RelatedNewsProps {
 
 const RelatedNews: React.FC<RelatedNewsProps> = ({ relatedArticles }) => {
   if (relatedArticles.length === 0) {
-    return <p className="text-gray-500 text-sm">Tidak ada berita terkait.</p>;
+    return <p className="text-gray-500 text-sm">Tidak ada berita terbaru.</p>;
   }
 
   return (
     <div className="bg-gray-50 rounded-lg shadow p-4 space-y-4">
-      <h2 className="font-bold text-lg mb-3 border-b pb-2">Berita Terkait</h2>
+      <h2 className="font-bold text-lg mb-3 border-b pb-2">Berita Terbaru</h2>
       <div className="flex flex-col space-y-4">
         {relatedArticles.map((article) => (
           <MiniNewsCard
