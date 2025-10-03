5f9135d5d6fba75bb99b350391b64124d2ffcbad
dimasbe <bedimas55@gmail.com>
2025-10-03 14:53:14 +0700

    add env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api-getskill.mijurnal.com/",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: { "Content-Type": "application/json" },
 });
 
